1a486dbba31ff1e7008d4519657ff83633996a59
Sumit Maurya <sumitmaurya746@gmail.com>
2026-01-03 07:36:47 +0530

    Initial commit

---
+++ new.js
@@ -0,0 +1,5 @@
+// Made this file to test JS in terminal using node.js bacause the other file have things like prompt 
+// and confirm that can only be opened in a browser
+console.log("Hello World...")
+console.log("This is Node.js")
+console.log("Working fine...")
---
+++ script.js
@@ -0,0 +1,17 @@
+// linked with index.html
+alert("Hey this is an alert")
+// open console
+console.log("This is the first line")
+console.log("This is the second line")
+console.log("This is the third line")
+// input and print
+var a = prompt("Enter your number:")
+console.log("Your number is: "+ a)
+//condition prompt
+var isTrue = confirm("Are you gay?" )
+if (isTrue){
+    console.log("YOU ARE GAY XD")
+}
+else{
+    console.log("YOU ARE NOT GAY")
+}
