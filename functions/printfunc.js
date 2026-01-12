2f312b921730232e3d461bdcba772531a42bce3e
Sumit Maurya <sumitmaurya746@gmail.com>
2026-01-12 23:07:00 +0530

    Added a small code

--- functions/printfunc.js
+++ functions/printfunc.js
@@ -3,7 +3,7 @@ function print(name) {
     console.log(name + " is learning javascript");
     console.log( name + " will push the code on github too");
     console.log(name + " desperately wants to travel the world");
-    console.log("The developer name is "+ name);
+    console.log("The developer's name is "+ name);
     
 }
 print("Rahul");
