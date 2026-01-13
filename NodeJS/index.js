f810d91cb91d4b024142723a70fcb34c674e42e0
Sumit Maurya <sumitmaurya746@gmail.com>
2026-01-13 11:14:10 +0530

    Pushing NodeJS folder

---
+++ NodeJS/index.js
@@ -0,0 +1,6 @@
+console.log("Helloooo World how are you");
+
+// now we want to access the contents on second.js in index.js
+
+const apple = require("./second"); //apple is just to store and print data, you can name it anything
+console.log(apple);
---
+++ NodeJS/second.js
@@ -0,0 +1,13 @@
+// making an object 
+
+sumit = {
+    name: "Sumit",
+    age : 20,
+    Developer: true
+
+}
+
+// now we will import this object in index.js 
+// we use the export function in this file and import func in index.js file
+
+module.exports = sumit;  // use the same object name
