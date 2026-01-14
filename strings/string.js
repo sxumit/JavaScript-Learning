f98725fed95547ae662b5e7dc5c9a1031fe031ab
Sumit Maurya <sumitmaurya746@gmail.com>
2026-01-14 21:51:47 +0530

    add Strings folder

---
+++ strings/string.js
@@ -0,0 +1,9 @@
+let a = "Sumit" // this creates a string, must be in double quotes
+console.log(a)
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(typeof(a)) // tells about the datatype of a 
+console.log(a.length) //this tells the total characters in the string. no use of bracket() after length
