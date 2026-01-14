db53984cc24228683439588c0c06f44d5565d39d
Sumit Maurya <sumitmaurya746@gmail.com>
2026-01-14 22:25:20 +0530

    updated slicing.js

--- strings/slicing.js
+++ strings/slicing.js
@@ -10,3 +10,13 @@ let b = "abrakadabra"
 console.log(b.replace("dabra", "bhangra"))
 //remember , if there are 2 occurences then only the first occurence will be replaced
 // if 'dabra' comes twice in out string, only first will be replaced
+
+let c = "Inter"
+let d = "stellar"
+console.log(c.concat(d))
+console.log(c.concat(d, " by Christopher Nolan")) 
+
+//strings are immutable, we are making new strings, not changing the existing string
+
+console.log(c.charAt(1)) // returns the character at index
+console.log(c.indexOf("te")) // returns the index at which the "te" is present
